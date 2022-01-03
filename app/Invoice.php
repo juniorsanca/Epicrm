4200a82e98d0a3e142fcf65f85694f05519c5db0
Junior <duartejusanca@gmail.com>
2022-01-03 13:11:26 +0100

    invoices

--- app/Invoice.php
+++ app/Invoice.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
+    //use HasFactory;
 
     protected $fillable = [
         'invoice_title',
